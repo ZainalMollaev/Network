a4f59087f0ec1fc620738d1265cc3aff7d847e9e
ZainalMollaev <mollaev.fortech@gmail.com>
2023-12-04 09:51:01 +0300

    security added

--- src/main/java/org/education/network/db/repository/UserRepository.java
+++ src/main/java/org/education/network/db/repository/UserRepository.java
@@ -2,7 +2,6 @@ package org.education.network.db.repository;
 
 import org.education.network.db.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
