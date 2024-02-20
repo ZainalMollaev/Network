aa56397fd020c286c2f3f38a83c190941ce41f94
ZainalMollaev <mollaev.fortech@gmail.com>
2024-02-20 18:31:54 +0300

    refractor(controllers): вернуть создание repsponseenity контроллеру

--- src/main/java/org/education/network/service/SubscribeService.java
+++ src/main/java/org/education/network/service/SubscribeService.java
@@ -1,11 +1,12 @@
 package org.education.network.service;
 
 import lombok.RequiredArgsConstructor;
-import org.education.network.util.ResponseEntityUtil;
+import org.education.network.dto.response.SubscriptionDto;
+import org.education.network.model.repository.UserProfileRepository;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,18 +15,17 @@ public class SubscribeService {
     private final UserProfileService userProfileService;
 
     //todo нельзя подписываться на самого себя
-    public ResponseEntity subscribeUser(String personEmail, String subscriptionEmail) {
+    public String subscribeUser(String personEmail, String subscriptionEmail) {
         userProfileService.subcribeUser(personEmail, subscriptionEmail);
-        return ResponseEntityUtil.get(HttpStatus.OK, "You successfully subscribed");
+        return "You successfully subscribed";
     }
 
-    public ResponseEntity getAllUserSubscriptions(String email, Pageable pageable) {
-        return ResponseEntityUtil.get(HttpStatus.OK, userProfileService.getAllUserSubscribers(email, pageable));
+    public List<SubscriptionDto> getAllUserSubscriptions(String email, Pageable pageable) {
+        return userProfileService.getAllUserSubscribers(email, pageable);
     }
 
-    //todo Сделать ResponseEntityUtil класс
-    public ResponseEntity findProperSubscriptionsOrSubscribersByName(String username, String likePattern) {
-        return ResponseEntityUtil.get(HttpStatus.OK, userProfileService.findProperSubscriptionsOrSubscribersByName(username, likePattern));
+    public List<UserProfileRepository.NameOnly> findProperSubscriptionsOrSubscribersByName(String username, String likePattern) {
+        return userProfileService.findProperSubscriptionsOrSubscribersByName(username, likePattern);
     }
 
 }
--- src/main/java/org/education/network/web/controller/SubscribeController.java
+++ src/main/java/org/education/network/web/controller/SubscribeController.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.education.network.service.SubscribeService;
+import org.education.network.util.ResponseEntityUtil;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,19 +29,19 @@ public class SubscribeController {
     @PutMapping
     public ResponseEntity subscribe(@RequestParam("personEmail") String personEmail,
                                     Principal principal) {
-        return subscribeService.subscribeUser(personEmail, principal.getName());
+        return ResponseEntityUtil.get(HttpStatus.OK, subscribeService.subscribeUser(personEmail, principal.getName()));
     }
 
     @Operation(summary = "get all user subscriptions")
     @GetMapping
     public ResponseEntity getAllUserSubscriptions(Principal principal, Pageable pageable) {
-        return subscribeService.getAllUserSubscriptions(principal.getName(), pageable);
+        return ResponseEntityUtil.get(HttpStatus.OK, subscribeService.getAllUserSubscriptions(principal.getName(), pageable));
     }
 
     @Operation(summary = "find subscriptions or subscribers by string")
     @GetMapping("/{likePattern}")
     public ResponseEntity findProperSubscriptionsOrSubscribersByString(Principal principal, @PathVariable String likePattern) {
-        return subscribeService.findProperSubscriptionsOrSubscribersByName(principal.getName(), likePattern);
+        return ResponseEntityUtil.get(HttpStatus.OK, subscribeService.findProperSubscriptionsOrSubscribersByName(principal.getName(), likePattern));
     }
 
 }
